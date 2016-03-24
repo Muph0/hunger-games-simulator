ffd5b0bfe0cac905eb3ba7d310f216863fd5a9f5
Muph0 <kytka.honza@gmail.com>
2016-03-24 16:24:00 +0100

    Added MapPreviewMenu

--- hunger-games-simulator/ui/MapMenu.cs
+++ hunger-games-simulator/ui/MapMenu.cs
@@ -8,14 +8,20 @@ using ConsoleBufferApi;
 
 namespace hunger_games_simulator.ui
 {
-    class MapMenu
+    class MapPreviewMenu
     {
         public static void Show(GameServer server)
         {
             for (int i = 0; true; i++)
             {
-                server.Arena = ArenaGenerator.Generate(server.GameAssets, i, 20);
-                ConsoleBuffer buf = server.Arena.MapBuffer();
+                server.Arena = ArenaGenerator.Generate(server.GameAssets, i, 30);
+                ConsoleBuffer buf = new ConsoleBuffer();
+                buf.InsertBuffer(server.Arena.MapBuffer(), 0, 0);
+                buf.SetCursorPosition(50, 0);
+                buf.WriteVertical("".PadRight(25, '▌'));
+                buf.SetCursorPosition(50, 0);
+                buf.Write("▌".PadRight(30, '-'));
+                
                 buf.DrawSelf();
                 Console.ReadKey(true);
             }
