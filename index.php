6adb000823999ce70203df91dad7d32c2d8ae712
Muph0 <kytka.honza@gmail.com>
2016-08-10 15:43:29 +0200

    Added backspace consumption & pixelated rendering

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Hunger games sim</title>
+    <title>Hunger Games Sim</title>
 
     <script type="text/javascript" src="./jquery.min.js"></script>
     <script type="text/javascript" src="./CanvasConsole/CanvasConsole.js"></script>
+    <meta charset="utf-8">
 
 <?php
     // TEMPORARY:
@@ -27,11 +28,18 @@
     }
     ?>
     <style type="text/css">
-        * { margin: 0; padding: 0; }
-        body { background-color: #111; }
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        body {
+            image-rendering: pixelated;
+            background-color: #111;
+        }
     </style>
 </head>
-<body onload="main(this)" onkeydown="Keyboard.keydown(event)" onkeyup="Keyboard.keyup(event)">
+<body onload="main(this)" onkeydown="Keyboard.keydown(event)" onkeyup="Keyboard.keyup(event)" onkeypress="Keyboard.keypress(event)">
+    <input type="text" style="opacity: 0; position: absolute; top: 0; left: 0;" autofocus>
     <img id="ascii" src="CanvasConsole/ascii.png" style="display: none;">
 </body>
 </html>
