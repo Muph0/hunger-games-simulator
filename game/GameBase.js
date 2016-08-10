e5b400732815aad1760705b8a1715cd56a8caafd
Muph0 <kytka.honza@gmail.com>
2016-08-10 15:28:01 +0200

    Added render manager

--- game/GameBase.js
+++ game/GameBase.js
@@ -1,54 +1,49 @@
 
-function GameBase(_Console)
+function GameBase(Console)
 {
     var self = this;
-    var Console = _Console;
-    var MainMenu = new Menu(["Start","Options","Allahu akbar","Exit"], Console);
     var kbs = [], lkbs = [];
+    self.render_mgr = new RenderManager(Console, self);
+
+    self.Time = 0;
 
     self.Start = function()
     {
+        Biome.RegisterBiomes();
+
         window.requestAnimationFrame(self.Run);
     }
 
     self.Run = function(time)
     {
-        self.Update(time);
-        self.Draw(time);
+        self.Time = time;
+        self.Update();
+        self.Draw();
         if (true) window.requestAnimationFrame(self.Run);
     }
 
-    self.Draw = function(time)
+    self.IsKeyDown = function(key)
     {
-        Console.Clear();
-        for (var key in kbs)
-        {
-            if(kbs[key])
-                Console.WriteLine(key);
-        }
-
-
-        Console.SetCursor(Math.floor(Math.sin(time/300)*10 + 40), Math.floor(Math.cos(time/300)*5 + 12));
-        Console.Write('x');
-
-        if (!kbs[Keyboard.Keys.Spacebar] && lkbs[Keyboard.Keys.Spacebar])
-        {
-            var r = Math.floor(Math.random() * 20);
-            var g = Math.floor(Math.random() * 20);
-            var b = Math.floor(Math.random() * 20);
-
-            Console.Background = [r,g,b];
-        }
-        Console.SetCursor(5,1);
-        MainMenu.Draw();
-
-
+        return kbs[key];
     }
-    self.Update = function(time)
+    self.IsKeyPressed = function(key)
+    {
+        return kbs[key] && !lkbs[key];
+    }
+    self.IsKeyReleased = function(key)
+    {
+        return !kbs[key] && lkbs[key];
+    }
+
+    self.Update = function()
     {
         lkbs = kbs;
         kbs = Keyboard.GetState();
-        MainMenu.Update();
 
+        self.render_mgr.Update();
+    }
+    self.Draw = function()
+    {
+        self.render_mgr.Draw();
     }
 }
